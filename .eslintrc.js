1695226d46607a10d69c07725fd73464e5132166
bertyn <bertynboulikou@gmail.com>
2021-04-04 10:54:43 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  }
+  ,
+  globals: {
+    $nuxt: true
+  }
+}
---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <nav class="flex item-center justify-between py-3">
+      <div>
+        <img src="" class="" alt="" />
+      </div>
+      <div>
+        <NuxtLink to="/" />
+      </div>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
