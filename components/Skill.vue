75466f0e4574cb9678aa669ef627503cbf856e78
bertyn <bertynboulikou@gmail.com>
2021-04-05 14:58:42 +0200

    correction bug responsivité  de  la section skill

---
+++ components/Skill.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="py-32 leading-6 text-left text-gray-900 box-border md:pb-12">
+    <div class="px-4 mx-auto w-full">
+      <div class="flex flex-wrap -mx-4 text-gray-900 box-border">
+        <div
+          class="relative px-4 w-full xl:flex-grow-0 xl:flex-shrink-0 xl:max-w-full"
+          style="min-height: 1px"
+        >
+          <div class="mb-16 text-center">
+            <span
+              class="block relative pt-3 mb-3 text-xs font-light leading-5 uppercase"
+              style="z-index: 3; letter-spacing: 3px"
+              >Service Fourni</span
+            >
+            <h3
+              class="relative z-0 pb-4 mt-0 mb-2 font-sans text-5xl font-bold md:text-4xl"
+              style="line-height: 62px"
+            >
+              Project digitale
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap px-1 text-gray-900 box-border">
+        <div
+          class="relative px-4 w-full md:w-1/3 xl:flex-grow-0 xl:flex-shrink-0 md:flex-grow-0 md:flex-shrink-0 box-border"
+          style="min-height: 1px"
+        >
+          <div
+            class="py-12 px-8 mb-8 text-center border border-gray-300 border-solid md:px-5 md:py-8 box-border hover:border-indigo-600"
+            style="transition: all 0.3s ease 0s"
+          >
+            <div class="box-border">
+              <img
+                src="icon/1.svg"
+                alt=""
+                class="object-center mx-auto border-none box-border"
+              />
+            </div>
+            <h3
+              class="pb-3 mt-6 mb-2 font-sans text-2xl font-semibold leading-5 md:text-xl box-border"
+            >
+              Creation de website
+            </h3>
+            <p
+              class="my-0 font-sans text-base font-normal leading-7 text-gray-600 box-border"
+            >
+              Elabore des landing page, blog, site d'e-commerce et d'autre idée
+              de website que vous auriez.
+            </p>
+          </div>
+        </div>
+        <div
+          class="relative px-4 w-full md:w-1/3 xl:flex-grow-0 xl:flex-shrink-0 md:flex-grow-0 md:flex-shrink-0 box-border"
+          style="min-height: 1px"
+        >
+          <div
+            class="py-12 px-8 mb-8 text-center border border-gray-300 border-solid md:px-5 md:py-8 box-border hover:border-indigo-600"
+            style="transition: all 0.3s ease 0s"
+          >
+            <div class="box-border text-center">
+              <img
+                src="icon/2.svg"
+                alt=""
+                class="object-center mx-auto border-none box-border"
+              />
+            </div>
+            <h3
+              class="pb-3 mt-6 mb-2 font-sans text-2xl font-semibold leading-5 md:text-xl box-border"
+            >
+              Web design
+            </h3>
+            <p
+              class="my-0 font-sans text-base font-normal leading-7 text-gray-600 box-border"
+            >
+              Réalise des maquettes de vos site web ou application qui donneront
+              vie a vos projet
+            </p>
+          </div>
+        </div>
+        <div
+          class="relative px-4 w-full md:w-1/3 xl:flex-grow-0 xl:flex-shrink-0 md:flex-grow-0 md:flex-shrink-0 box-border"
+          style="min-height: 1px"
+        >
+          <div
+            class="py-12 px-8 mb-8 text-center border border-gray-300 border-solid md:px-5 md:py-8 box-border hover:border-indigo-600"
+            style="transition: all 0.3s ease 0s"
+          >
+            <div class="box-border">
+              <img
+                src="icon/3.svg"
+                alt=""
+                class="object-center mx-auto border-none box-border"
+              />
+            </div>
+            <h3
+              class="pb-3 mt-6 mb-2 font-sans text-2xl font-semibold leading-5 md:text-xl box-border"
+            >
+              Web &amp; Mobile app
+            </h3>
+            <p
+              class="my-0 font-sans text-base font-normal leading-7 text-gray-600 box-border"
+            >
+              Selon votre demande je pourrais réaliser des site web voir mes des
+              app cross platform
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
