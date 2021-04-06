945900637f35e5c5461c9662f0632f4324734921
bertyn <bertynboulikou@gmail.com>
2021-04-06 20:57:46 +0200

    ajout du composant project

---
+++ components/Project.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="relative pt-48 leading-6 text-left text-gray-900 bg-gray-900 box-border"
+    style="padding-bottom: 340px"
+  >
+    <div class="px-4 mx-auto w-full text-left box-border">
+      <div class="flex flex-wrap -mx-4 text-gray-900 box-border">
+        <div
+          class="relative px-4 w-full xl:flex-grow-0 xl:flex-shrink-0 xl:max-w-full box-border"
+          style="min-height: 1px"
+        >
+          <div class="text-center box-border">
+            <span
+              class="block relative pt-0 mb-3 text-xs font-light leading-5 text-white uppercase box-border"
+              style="z-index: 3; letter-spacing: 3px"
+              >Portfolios</span
+            >
+            <h3
+              class="relative z-0 pb-4 mt-0 mb-2 font-sans text-5xl font-bold text-white box-border md:text-4xl"
+              style="line-height: 62px"
+            >
+              Quelque projet que j'ai réalisé<br
+                class="box-border"
+                style="line-height: 62px"
+              />
+              Au cour de l'année
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
