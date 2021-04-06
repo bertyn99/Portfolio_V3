ffebbc0b15a87895101f5466904066a21a24c108
bertyn <bertynboulikou@gmail.com>
2021-04-06 20:53:28 +0200

    ajout du composant a propos

---
+++ components/AboutUs.vue
@@ -0,0 +1,152 @@
+<template>
+  <section id="about">
+    <div
+      class="relative pt-20 pb-40 leading-6 text-left text-gray-900 box-border lg:pb-24"
+    >
+      <div
+        class="hidden absolute top-0 font-extrabold leading-none text-gray-100 lg:block box-border"
+        style="font-size: 235px; left: 20%"
+      >
+        A propos
+      </div>
+      <div class="px-4 mx-auto w-full text-left box-border">
+        <div
+          class="flex flex-wrap items-center justify-center mx-4 text-gray-900 box-border"
+        >
+          <div
+            class="relative px-4 w-full text-center md:w-1/2 xl:flex-shrink-0 md:flex-shrink-0"
+            style="min-height: 1px"
+          >
+            <div class="box-border">
+              <h3
+                class="mt-0 mb-2 font-sans text-5xl font-bold leading-6 box-border"
+              >
+                A propos
+              </h3>
+              <p
+                class="mt-5 mb-8 font-sans text-sm font-normal leading-8 text-gray-600 box-border"
+              >
+                Etudiant en developpement web
+              </p>
+              <div class="box-border">
+                <a
+                  class="inline-block py-3 px-10 font-sans text-sm font-medium leading-5 text-center text-white no-underline capitalize bg-indigo-600 rounded-none border border-transparent border-solid cursor-pointer box-border hover:bg-transparent hover:border-indigo-600 hover:text-indigo-600 hover:no-underline focus:no-underline"
+                  href="cv\C.V job.pdf"
+                  download=""
+                  style="transition: all 0.5s ease 0s; outline: none medium"
+                  >Telecharger le CV</a
+                >
+              </div>
+            </div>
+          </div>
+          <div
+            class="relative px-4 w-full md:w-1/2 xl:flex-shrink-0 md:flex-shrink-0 box-border"
+            style="min-height: 1px"
+          >
+            <div class="relative box-border">
+              <div
+                class="hidden md:block absolute xl:right-0 lg:right-0 box-border"
+                style="right: 40px; top: -153px; z-index: -2"
+              >
+                <img
+                  src="background/color_grid.png"
+                  alt=""
+                  class="align-middle border-none"
+                />
+              </div>
+              <div class="box-border">
+                <img
+                  src="pose.jpg"
+                  alt=""
+                  class="w-full h-86 md:w-1/2 align-middle border-none box-border"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="pt-20 pb-32 leading-6 text-left text-gray-900 box-border">
+          <div class="px-4 mx-auto w-full text-left box-border">
+            <div class="flex flex-row -mx-4 text-gray-900 box-border">
+              <div
+                class="relative px-4 w-full md:w-1/3 xl:flex-shrink-0 md:flex-shrink-0 box-border"
+                style="min-height: 1px"
+              >
+                <div
+                  class="px-0 pt-12 pb-10 mb-8 text-center border border-gray-300 border-solid box-border hover:border-indigo-600"
+                  style="transition: all 0.3s ease 0s"
+                >
+                  <h3
+                    class="mt-0 mb-4 font-sans text-2xl font-medium leading-5 box-border"
+                  >
+                    <span
+                      class="text-5xl font-bold leading-7 text-indigo-600 box-border"
+                      >8</span
+                    ><span
+                      class="text-5xl font-bold leading-7 text-indigo-600 box-border"
+                      >+</span
+                    >
+                  </h3>
+                  <p
+                    class="my-0 font-sans text-sm font-normal leading-7 box-border"
+                  >
+                    Project au total
+                  </p>
+                </div>
+              </div>
+              <div
+                class="relative px-4 w-full md:w-1/3 xl:flex-shrink-0 md:flex-shrink-0 box-border"
+                style="min-height: 1px"
+              >
+                <div
+                  class="px-0 pt-12 pb-10 mb-8 text-center border border-gray-300 border-solid box-border hover:border-indigo-600"
+                  style="transition: all 0.3s ease 0s"
+                >
+                  <h3
+                    class="mt-0 mb-4 font-sans text-2xl font-medium leading-5 box-border"
+                  >
+                    <span
+                      class="text-5xl font-bold leading-7 text-indigo-600 box-border"
+                      >5</span
+                    >
+                  </h3>
+                  <p
+                    class="my-0 font-sans text-sm font-normal leading-7 box-border"
+                  >
+                    Projetc en cours
+                  </p>
+                </div>
+              </div>
+              <div
+                class="relative px-4 w-1/3 md:flex-shrink-0 box-border"
+                style="min-height: 1px"
+              >
+                <div
+                  class="px-0 pt-12 pb-10 mb-8 text-center border border-gray-300 border-solid box-border hover:border-indigo-600"
+                  style="transition: all 0.3s ease 0s"
+                >
+                  <h3
+                    class="mt-0 mb-4 font-sans text-2xl font-medium leading-5 box-border"
+                  >
+                    <span
+                      class="text-5xl font-bold leading-7 text-indigo-600 box-border"
+                      >25</span
+                    >
+                    <span
+                      class="text-5xl font-bold leading-7 text-indigo-600 box-border"
+                      >%</span
+                    >
+                  </h3>
+                  <p
+                    class="my-0 font-sans text-sm font-normal leading-7 box-border"
+                  >
+                    Projet fini
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
