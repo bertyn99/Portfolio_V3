24aa426a93c9684ca89a432da442a8a5a967d947
bertyn <bertynboulikou@gmail.com>
2021-04-06 20:56:51 +0200

    correction bug prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,27 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   extends: [
-    '@nuxtjs',
-    'plugin:vue/recommended',
-    'eslint:recommended',
-    'prettier/vue',
-    'plugin:prettier/recommended'
+    "@nuxtjs",
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier",
+    "plugin:prettier/recommended",
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
   rules: {
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  }
-  ,
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
   globals: {
-    $nuxt: true
-  }
-}
+    $nuxt: true,
+  },
+};
