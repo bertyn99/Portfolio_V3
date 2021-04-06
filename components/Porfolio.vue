9304a6fcf1ca98e45bc30a0e36a0a93df7639bac
bertyn <bertynboulikou@gmail.com>
2021-04-06 20:56:20 +0200

    ajout du composant portfolio

---
+++ components/Porfolio.vue
@@ -0,0 +1,222 @@
+<template>
+  <div
+    class="relative pb-20 leading-6 text-left text-gray-900 box-border"
+    style="margin-top: -295px"
+  >
+    <div class="px-4 mx-auto w-full text-left box-border">
+      <div class="flex flex-wrap -mx-4 text-gray-900 box-border">
+        <div
+          class="relative px-4 w-full xl:flex-grow-0 xl:flex-shrink-0 md:flex-grow-0 md:flex-shrink-0 box-border"
+          style="min-height: 1px"
+        >
+          <div class="overflow-hidden relative mb-5 box-border">
+            <div class="box-border" style="height: 410px">
+              <img
+                src="img/portfolio/1.png"
+                alt=""
+                class="w-full h-full align-middle border-none box-border"
+              />
+            </div>
+            <a
+              href="img/portfolio/1.png"
+              class="absolute top-0 left-0 w-full h-full no-underline bg-transparent cursor-pointer box-border hover:text-blue-700 hover:no-underline focus:no-underline"
+              style="
+                transition: all 0.3s ease 0s;
+                outline: none medium;
+                z-index: 9;
+              "
+            ></a>
+            <div
+              class="overflow-hidden absolute top-0 left-0 w-full h-full text-center bg-purple-400 opacity-0 box-border hover:opacity-100"
+              style="transform: translateX(-50%); transition: all 0.5s ease 0s"
+            >
+              <div
+                class="absolute inset-x-0 box-border"
+                style="top: 50%; transform: translateY(-50%)"
+              >
+                <h3
+                  class="mt-0 mb-2 font-sans text-2xl font-bold leading-5 text-white box-border"
+                >
+                  Appli mobile TedX
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="relative px-4 w-full xl:flex-grow-0 xl:flex-shrink-0 md:flex-grow-0 md:flex-shrink-0 box-border"
+          style="min-height: 1px"
+        >
+          <div class="overflow-hidden relative mb-5 box-border">
+            <div class="box-border" style="height: 410px">
+              <img
+                src="img/portfolio/Mishi Logo V1.png"
+                alt=""
+                class="w-full h-full align-middle border-none box-border"
+              />
+            </div>
+            <a
+              href="img/portfolio/2.png"
+              class="absolute top-0 left-0 w-full h-full no-underline bg-transparent cursor-pointer box-border hover:text-blue-700 hover:no-underline focus:no-underline"
+              style="
+                transition: all 0.3s ease 0s;
+                outline: none medium;
+                z-index: 9;
+              "
+            ></a>
+            <div
+              class="overflow-hidden absolute top-0 left-0 w-full h-full text-center bg-purple-400 opacity-0 box-border hover:opacity-100"
+              style="transform: translateX(-50%); transition: all 0.5s ease 0s"
+            >
+              <div
+                class="absolute inset-x-0 box-border"
+                style="top: 50%; transform: translateY(-50%)"
+              >
+                <h3
+                  class="mt-0 mb-2 font-sans text-2xl font-bold leading-5 text-white box-border"
+                >
+                  Site d'e-commerce
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="relative px-4 w-full xl:flex-grow-0 xl:flex-shrink-0 md:flex-grow-0 md:flex-shrink-0 lg:flex-grow-0 lg:flex-shrink-0 box-border"
+          style="min-height: 1px"
+        >
+          <div class="overflow-hidden relative mb-5 box-border">
+            <div class="box-border" style="height: 410px">
+              <img
+                src="img/portfolio/3.png"
+                alt=""
+                class="w-full h-full align-middle border-none box-border"
+              />
+            </div>
+            <a
+              href="img/portfolio/3.png"
+              class="absolute top-0 left-0 w-full h-full no-underline bg-transparent cursor-pointer box-border hover:text-blue-700 hover:no-underline focus:no-underline"
+              style="
+                transition: all 0.3s ease 0s;
+                outline: none medium;
+                z-index: 9;
+              "
+            ></a>
+            <div
+              class="overflow-hidden absolute top-0 left-0 w-full h-full text-center bg-purple-400 opacity-0 box-border hover:opacity-100"
+              style="transform: translateX(-50%); transition: all 0.5s ease 0s"
+            >
+              <div
+                class="absolute inset-x-0 box-border"
+                style="top: 50%; transform: translateY(-50%)"
+              >
+                <h3
+                  class="mt-0 mb-2 font-sans text-2xl font-bold leading-5 text-white box-border"
+                >
+                  Product Design
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="relative px-4 w-full xl:flex-grow-0 xl:flex-shrink-0 md:flex-grow-0 md:flex-shrink-0 lg:flex-grow-0 lg:flex-shrink-0 box-border"
+          style="min-height: 1px"
+        >
+          <div class="overflow-hidden relative mb-5 box-border">
+            <div class="box-border" style="height: 410px">
+              <img
+                src="img/portfolio/maquette2.png"
+                alt=""
+                class="w-full h-full align-middle border-none box-border"
+              />
+            </div>
+            <a
+              href="img/portfolio/4.png"
+              class="absolute top-0 left-0 w-full h-full no-underline bg-transparent cursor-pointer box-border hover:text-blue-700 hover:no-underline focus:no-underline"
+              style="
+                transition: all 0.3s ease 0s;
+                outline: none medium;
+                z-index: 9;
+              "
+            ></a>
+            <div
+              class="overflow-hidden absolute top-0 left-0 w-full h-full text-center bg-purple-400 opacity-0 box-border hover:opacity-100"
+              style="transform: translateX(-50%); transition: all 0.5s ease 0s"
+            >
+              <div
+                class="absolute inset-x-0 box-border"
+                style="top: 50%; transform: translateY(-50%)"
+              >
+                <h3
+                  class="mt-0 mb-2 font-sans text-2xl font-bold leading-5 text-white box-border"
+                >
+                  Maquette de site web
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="relative px-4 w-full xl:flex-grow-0 xl:flex-shrink-0 md:flex-grow-0 md:flex-shrink-0 md:max-w-full lg:flex-grow-0 lg:flex-shrink-0 box-border"
+          style="min-height: 1px"
+        >
+          <div class="overflow-hidden relative mb-5 box-border">
+            <div class="box-border" style="height: 410px">
+              <img
+                src="img/portfolio/airbnblike.jpg"
+                alt=""
+                class="w-full h-full align-middle border-none box-border"
+              />
+            </div>
+            <a
+              href="img/portfolio/5.png"
+              class="absolute top-0 left-0 w-full h-full no-underline bg-transparent cursor-pointer box-border hover:text-blue-700 hover:no-underline focus:no-underline"
+              style="
+                transition: all 0.3s ease 0s;
+                outline: none medium;
+                z-index: 9;
+              "
+            ></a>
+            <div
+              class="overflow-hidden absolute top-0 left-0 w-full h-full text-center bg-purple-400 opacity-0 box-border hover:opacity-100"
+              style="transform: translateX(-50%); transition: all 0.5s ease 0s"
+            >
+              <div
+                class="absolute inset-x-0 box-border"
+                style="top: 50%; transform: translateY(-50%)"
+              >
+                <h3
+                  class="mt-0 mb-2 font-sans text-2xl font-bold leading-5 text-white box-border"
+                >
+                  Site web Airbnblike
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-4 text-gray-900 box-border">
+        <div
+          class="relative px-4 w-full xl:flex-grow-0 xl:flex-shrink-0 xl:max-w-full box-border"
+          style="min-height: 1px"
+        >
+          <div class="mt-16 text-center box-border">
+            <a
+              class="inline-block py-3 px-8 text-sm font-semibold text-indigo-600 no-underline capitalize bg-transparent border border-indigo-600 border-solid cursor-pointer box-border hover:bg-indigo-600 hover:text-white hover:no-underline focus:no-underline"
+              href="#"
+              style="transition: all 0.3s ease 0s; outline: none medium"
+              >More Folio</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Porfolio",
+  setup() {},
+};
+</script>
