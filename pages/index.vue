90784179ce4accff880e6fc31a63d877f570dbc5
bertyn <bertynboulikou@gmail.com>
2021-04-08 19:47:06 +0200

    retrait du css de base de nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <navbar />
+  <main>
+    <presentation></presentation>
+    <skill></skill>
+    <project></project>
+    <portfolio></portfolio>
+    <about-us></about-us>
+    <testimonies></testimonies>
+    <call-to-contact></call-to-contact>
+  </main>
 </template>
 
-<script>
-import Navbar from '@/components/Navbar.vue';
-export default {
-  components: { Navbar }
-};
-</script>
+<script></script>
 
 <style>
 /* Sample `apply` at-rules with Tailwind CSS
@@ -15,34 +18,4 @@ export default {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
